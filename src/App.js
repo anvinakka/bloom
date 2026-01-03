a9e3af341b24e5351c0789a87c832bc2b7ed4aba
anvinakka <anvithanakka05@gmail.com>
2026-01-03 16:25:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Onboarding from "./pages/Onboarding";
+import Dashboard from "./pages/Dashboard";
+
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
       </Routes>
     </Router>
   );
