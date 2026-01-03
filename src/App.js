8de99b4e5a1af2ca4fa91202935656573e38c618
anvinakka <anvithanakka05@gmail.com>
2026-01-03 16:49:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+import Layout from "./Layout";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Onboarding from "./pages/Onboarding";
@@ -10,23 +13,24 @@ import Journal from "./pages/Journal";
 import Learn from "./pages/Learn";
 import Guide from "./pages/Guide";
 
-
-
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Pages without nav */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/daily-checkin" element={<DailyCheckIn />} />
-        <Route path="/breathing" element={<Breathing />} />
-        <Route path="/journal" element={<Journal />} />
-        <Route path="/learn" element={<Learn />} />
-        <Route path="/guide" element={<Guide />} />
+        <Route path="/onboarding" element={<Onboarding />} />
 
-
-
+        {/* Pages WITH nav */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/daily-checkin" element={<DailyCheckIn />} />
+          <Route path="/breathing" element={<Breathing />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/learn" element={<Learn />} />
+          <Route path="/guide" element={<Guide />} />
+        </Route>
       </Routes>
     </Router>
   );
